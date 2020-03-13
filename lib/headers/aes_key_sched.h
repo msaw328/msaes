2c145fe4277d85e1159346c1ae144a828eedeb12
Maciej Sawka <maciejsawka@gmail.com>
2020-03-13 02:05:02 +0100

    Add support for all standarized key lengths
    Add another argument to aes_key_sched_generate() which determines
    AES key size used, number of rounds and other variables of
    operation. Also implement additional S-BOX present only for
    extension of 256 bit keys.
    
    Add aes_key_sched_wordbuffer_size() utility function which
    return buffer needed to contain all the round keys.

--- lib/headers/aes_key_sched.h
+++ lib/headers/aes_key_sched.h
@@ -6,6 +6,15 @@
 
 #include "aes_proc.h"
 
-void aes_key_sched_generate(uint8_t* key, uint32_t* key_sched);
+#define AES128 0
+#define AES192 1
+#define AES256 2
+
+#define AES128_KEYSCHED_WORDS 44
+#define AES192_KEYSCHED_WORDS 52
+#define AES256_KEYSCHED_WORDS 60
+
+// performs the actual key sched algo, reads from key writes to key_sched
+void aes_key_sched_generate(uint8_t* key, uint32_t* key_sched, int aes_mode);
 
 #endif
--- lib/src/aes_key_sched.c
+++ lib/src/aes_key_sched.c
@@ -2,20 +2,40 @@
 
 uint32_t const RCON[] = { 0x00, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36 };
 
-void aes_key_sched_generate(uint8_t* key, uint32_t* key_sched) {
-    int rounds = 11; // 11 rounds for 128 bit key
+void aes_key_sched_generate(uint8_t* key, uint32_t* key_sched, int aes_mode) {
+    int rounds;
+    int key_bytes;
+    switch(aes_mode) {
+        default:
+        case AES128:
+            rounds = 10;
+            key_bytes = 16;
+            break;
 
-    memcpy(key_sched, key, 16); // 16 bytes = 128 bits, copy key to the state
+        case AES192:
+            rounds = 12;
+            key_bytes = 24;
+            break;
 
-    uint32_t tmp;
-    for(int i = 4; i < rounds * 4; i++) {
-        tmp = key_sched[i - 1];
+        case AES256:
+            rounds = 14;
+            key_bytes = 32;
+            break;
+    }
 
-        if(i % 4 == 0) {
+    memcpy(key_sched, key, key_bytes); // copy original key to the beginning of key sched buffer
+
+    int mod = key_bytes / 4; // number of blocks when rcon is applied, how far to look for our last word etc. 4 for 128; 6 for 192; 8 for 256 
+    for(int i = mod; i < (rounds + 1) * 4; i++) { // +1 round for 0th round, aka round with the original key
+        uint32_t tmp = key_sched[i - 1];
+
+        if(i % mod == 0) {
             tmp = aes_proc_rot_word(tmp, 1);
-            tmp = aes_proc_sub_word(tmp) ^ RCON[i / 4];
+            tmp = aes_proc_sub_word(tmp) ^ RCON[i / mod];
+        } else if(aes_mode == AES256 && i % mod == 4) { // key size 256 gets special treatment - S-BOX applied on half of the repetition
+            tmp = aes_proc_sub_word(tmp);
         }
 
-        key_sched[i] = key_sched[i - 4] ^ tmp;
+        key_sched[i] = key_sched[i - mod] ^ tmp;
     }
 }
